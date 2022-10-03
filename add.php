7407070d057917ab481399774cf357b3a588fc39
haikalrozhan <haikalrozhan1992@yahoo.com>
2022-10-03 14:45:54 +0800

    make add form and table for posts

---
+++ add.php
@@ -0,0 +1,30 @@
+<?php include("header.php"); ?>
+
+<div class="container">
+    <div class="row justify-content-center mt-5">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Add Post</div>
+                <div class="card-body">
+                    <div class="form-group">
+                        <label for="title">Title</label>
+                        <input type="text" name="title" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="description">Description</label>
+                        <textarea id="editor" type="text" name="description" class="form-control"></textarea>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+    ClassicEditor
+        .create(document.querySelector('#editor'))
+        .catch(error => {
+            console.error(error);
+        });
+</script>
